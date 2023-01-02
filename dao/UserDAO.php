d7ada09579d3365b68d101e444a719c36e2e5526
bywilliams <willian.seu@gmail.com>
2023-01-02 20:44:27 -0300

    User Model and UserDAO Interface

---
+++ dao/UserDAO.php
@@ -0,0 +1,37 @@
+<?php  
+
+require_once("models/User.php");
+
+    class UserDAO implements UserDAOInterface {
+
+        public function buildUser($data)  {
+
+        }
+        public function create(User $user, $authUser = false) {
+
+        }
+        public function update(User $user) {
+
+        }
+        public function verifyToken($protected = false) {
+
+        }
+        public function setTokenSession($token, $redirect = true) {
+
+        }
+        public function authenticatorUser($email, $password) {
+
+        }
+        public function findByEmail($email) {
+
+        }
+        public function findById($id) {
+
+        }
+        public function findByToken($token) {
+
+        }
+        public function changePassword(User $user) {
+
+        }
+    }
--- models/User.php
+++ models/User.php
@@ -13,16 +13,16 @@ Class User {
 }
 
 interface UserDAOInterface {
-
     public function buildUser($data); #irá construir o objeto
-    public function create(User $user, $authUser = false); #irá criar o usuario e já fez login
+    public function create(User $user, $authUser = false); #irá criar o usuario e já fazer login
     public function update(User $user); #irá atualizar usuário no sistema
-    public function findByToken($token); #irá encontrar usuário no sistema pelo token
     public function verifyToken($protected = false); # irá verificar usuário
     public function setTokenSession($token, $redirect = true); # irá fazer o login
-    public function authenticatorUser($email, $password); #junto com setTokenSession irá efetuar o login
+    public function authenticatorUser($email, $password); #junto com setTokenSession irá efetuar autenticação
     public function findByEmail($email); #irá encontrar usuário por email
     public function findById($id); #irá encontrar usuário pelo ID
+    public function findByToken($token); #irá encontrar usuário no sistema pelo token
+    public function changePassword(User $user); #irá mudar o password
 }
 
 
